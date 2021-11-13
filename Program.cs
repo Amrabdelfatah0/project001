9c3da930a62fadabd428b5619096b4b4c914f0b9
Amr Mohamed <88450778+Amrabdelfatah0@users.noreply.github.com>
2021-11-13 22:14:16 +0200

    ll

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace project001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//i dont understand this cod
+            user User = new user();
+
+            Console.WriteLine("first name");
+            User.firstName = Console.ReadLine();
+
+            Console.WriteLine("last name");
+            User.lastname = Console.ReadLine();
+
+            Console.WriteLine("username");
+            User.username = Console.ReadLine();
+
+            Console.WriteLine("password");
+            User.password = Console.ReadLine();
+
+            Console.WriteLine("confirm your password");
+            User.confirmpassword = Console.ReadLine();
+            if (User.password == User.confirmpassword)
+            {
+                Console.WriteLine("your birthday");
+                User.birthDay = Convert.ToDateTime(Console.ReadLine());
+
+                if (User.CalAge() > 18)
+                {
+                    Console.WriteLine(" your email");
+                    User.email = Console.ReadLine();
+
+                    Console.WriteLine("your month salary");
+                    User.monthsalary = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("your mobile number");
+                    User.mobaile = Console.ReadLine();
+                    if (User.mobaile.Length == 14)
+                    {
+                        Console.WriteLine("write your nationalty");
+                        User.nationalid = Console.ReadLine();
+
+                        Console.WriteLine(User.FullName());
+                        Console.WriteLine("your Age is : " + User.CalAge());
+                        Console.WriteLine( "your yearly salary " + User.yearly_salary());
+                    }
+                    else
+                    {
+                        Console.WriteLine("inter your corect number");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("inter your corect Age");
+                }
+
+               
+            } 
+            else
+            {
+                Console.WriteLine("chek your password");
+            }
+
+
+
+
+
+        }
+    }
+}
+
---
+++ user.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace project001
+{
+    public class user
+    {
+        public string firstName;
+        public string lastname;
+        public string username;
+        public string password;
+        public string confirmpassword;
+        public DateTime birthDay;
+        public string email;
+        public int monthsalary;
+        public string mobaile;
+        public string nationalid;
+
+        private string Name;
+        public string FullName()
+        {
+           
+            Name = $"User full name is: {firstName} {lastname}";
+            
+            return Name;
+        }
+
+        private DateTime today = DateTime.Now;
+        private int Age;
+
+        public int CalAge()
+        {
+            Age = today.Year - birthDay.Year;
+            return Age;
+        }
+
+        private int yearly;
+        public int yearly_salary()
+        {
+           int yearly = monthsalary*12;
+            return yearly;
+        }
+
+    }
+
+
+}
